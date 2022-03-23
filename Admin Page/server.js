9ab8c1840ad8369dc0e13706fb5952f3853383a7
Marcus <marcushvingelby@gmail.com>
2022-03-23 17:54:16 +0100

    Initial commit

---
+++ Admin Page/server.js
@@ -0,0 +1,54 @@
+//#region Express Setup
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(express.static(__dirname + '/public'))
+//#endregion
+
+const mongoose = require('mongoose')
+const product = require('../User Page/models/product')
+
+mongoose.connect('mongodb://127.0.0.1/testWebShop', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/addProduct', async (req, res) => {
+    const productName = req.body.productName
+    const productPrice = req.body.productPrice
+    const productDescription = req.body.productDescription
+
+    // If the above variables aren't set, return
+    if (!productName || !productPrice || !productDescription) return res.redirect('/')
+    
+    try {
+        const amountOfProducts = product.length
+
+        await product.create({
+            id: amountOfProducts + 1,
+            name: productName,
+            price: productPrice,
+            img_src: "",
+            description: productDescription
+        })
+
+        res.send("Name: " + productName + " Price: " + productPrice + "\nDescription: " + productDescription)
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+})
+
+
+app.listen('8000')
---
+++ User Page/models/product.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+    },
+
+    name: {
+        type: String,
+        required: true,
+    },
+
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
+    img_src: {
+        type: String,
+        default: "https://via.placeholder.com/250/",
+        required: false,
+    },
+
+    description: {
+        type: String,
+        required: true,
+    },
+})
+
+module.exports = mongoose.model('product', productSchema)
